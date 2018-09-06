88c2cfcf41faf45c0338e53e339e09bc475ad74b
Luis <ladm2110@gmail.com>
2018-09-06 11:13:36 -0400

    Processing of image moved to server side. Sending canvas data instead.

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -6,18 +6,23 @@ var clickDrag = new Array();
 var paint = false;
 var pixels;
 
+
 function addClick(x, y, dragging){
     clickX.push(x);
     clickY.push(y);
     clickDrag.push(dragging);
 }
 
+
 function redraw(){
     context.clearRect(0, 0, 280, 280);
 
     context.strokeStyle = "#000000";
+    context.fillStyle = "#ffffff"
     context.lineJoin = "round";
-    context.lineWidth = 10;
+    context.lineWidth = 20;
+
+    context.fillRect(0, 0, 280, 280)
 
     for(var i=0; i<clickX.length; i++){
         
@@ -32,8 +37,8 @@ function redraw(){
     }
 }
 
+
 function prepare_canvas(){
-    
     $('#drawing_canvas').mousedown(function(e){
         paint = true;
         rect = canvas.getBoundingClientRect();
@@ -59,6 +64,7 @@ function prepare_canvas(){
     });
 }
 
+
 function prepare_clear(){
     $('#clear_btn').click(function(){
         context.clearRect(0, 0, 280, 280);
@@ -70,86 +76,13 @@ function prepare_clear(){
     });
 }
 
+
 function prepare_predict(){
     $('#prediction_btn').click(function(){
-        pixels = new Array(28*28);
-        for(var j=0; j<784; j++){
-            pixels[j] = 0;
-        }
-        for(var i=0; i<clickX.length; i++){
-            x = parseInt(clickX[i]/10);
-            y = parseInt(clickY[i]/10);
-            len = 28;
-            weight = 255.0 / 5; //%9
-            positions = get_positions(x, y, len);
-            for(var k=0; k<positions.length; k++){
-                if(pixels[positions[k]] + weight < 255.0){
-                    pixels[positions[k]] += weight;
-                }else{
-                    pixels[positions[k]] = 255.0
-                }
-                
-            }
-        }
-        api_call(pixels)
-        //api_call(canvas.toDataURL())
+        api_call(canvas.toDataURL())
     });
 }
 
-function get_positions(x, y, len){
-    // SQUARED
-    // 1 2 3
-    // 4 5 6
-    // 7 8 9
-    // x,y = (5)
-    var positions = new Array();
-    var val;
-    //1
-    if(x>0 && y>0){
-        val = len * (y-1) + (x-1);
-        positions.push(val); 
-    }
-    //2
-    if(y>0){
-        val = len * (y-1) + x;
-        positions.push(val); 
-    }
-    //3
-    if(x<len-1 && y>0){
-        val = len * (y-1) + (x+1);
-        positions.push(val); 
-    }
-    //4
-    if(x>0){
-        val = len * y + (x-1);
-        positions.push(val); 
-    }
-    //6
-    if(x<len-1){
-        val = len * y + (x+1);
-        positions.push(val);
-    }
-    //7
-    if(x>0 && y<len-1){
-        val = len * (y+1) + (x-1);
-        positions.push(val); 
-    }
-    //8
-    if(y<len-1){
-        val = len * (y+1) + x;
-        positions.push(val); 
-    }
-    //9
-    if(x<len-1 && y<len-1){
-        val = len * (y+1) + (x+1);
-        positions.push(val); 
-    }
-    //5
-    val = len * y + x;
-    positions.push(val);
-
-    return positions;
-}
 
 $(document).ready(function(){
     prepare_canvas();
