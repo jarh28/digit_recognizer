fcab97d5dbeec3ce59d13a637af89b4f5e67bfe4
Luis <ladm2110@gmail.com>
2018-09-06 11:10:05 -0400

    Use of opencv to process the image in server

--- serve.py
+++ serve.py
@@ -1,20 +1,25 @@
 import numpy as np
 from model import load_model
-import pandas as pd
-import os
+import base64
+import cv2
+
 
 def get_model_api():
     model = load_model.load_model()
 
     def model_api(input_data):
-        x = np.array(input_data, dtype=float)
+        data = str(input_data)
+        img_data = data.split(',')[1]
+        img_decoded = base64.b64decode(img_data)
 
-        data = pd.Series(x, name='N')
-        _dir = os.path.join(os.path.dirname(__file__), 'number.csv')
-        data.to_csv(_dir, index=False)
+        np_img = np.fromstring(img_decoded, np.uint8)
+        img = cv2.imdecode(np_img, cv2.IMREAD_GRAYSCALE)
 
-        x /= 255.0
-        x = x.reshape((1,len(x)))
+        img_resized = cv2.resize(255-img, (28, 28))
+
+        x = img_resized.flatten() / 255.0
+        x = x.reshape((1, len(x)))
+
         prediction = model.predict_classes(x)
         return prediction[0]
 
