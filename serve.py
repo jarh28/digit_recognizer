ad527b327f11e240d2c825d7d93b67445a5d9586
Luis <ladm2110@gmail.com>
2018-09-06 21:26:31 -0400

    Use of OpenCv for processing the image, Padding for completion.

--- serve.py
+++ serve.py
@@ -2,6 +2,8 @@ import numpy as np
 from model import load_model
 import base64
 import cv2
+import math
+import pandas as pd
 
 
 def get_model_api():
@@ -15,11 +17,51 @@ def get_model_api():
         np_img = np.fromstring(img_decoded, np.uint8)
         img = cv2.imdecode(np_img, cv2.IMREAD_GRAYSCALE)
 
-        img_resized = cv2.resize(255-img, (28, 28))
+        img = 255-img
+
+        # Top row
+        while np.sum(img[0]) == 0:
+            img = img[1:]
+
+        # Bottom row
+        while np.sum(img[-1]) == 0:
+            img = img[:-1]
+
+        # First column
+        while np.sum(img[:, 0]) == 0:
+            img = np.delete(img, 0, 1)
+
+        # Last column
+        while np.sum(img[:, -1]) == 0:
+            img = np.delete(img, -1, 1)
+
+        rows, cols = img.shape
+
+        # Resize with a factor
+        if rows > cols:
+            factor = 20.0 / rows
+            rows = 20
+            cols = int(round(cols * factor))
+            img = cv2.resize(img, (cols, rows))
+        else:
+            factor = 20.0 / cols
+            cols = 20
+            rows = int(round(rows * factor))
+            img = cv2.resize(img, (cols, rows))
+
+        # Padding to complete the 28x28 dims
+        cols_padding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
+        rows_padding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
+        img = np.lib.pad(img, (rows_padding, cols_padding), 'constant')
+
+        # img_resized = cv2.resize(255-img, (28, 28))
+        img_resized = img
 
         x = img_resized.flatten() / 255.0
-        x = x.reshape((1, len(x)))
 
+        pd.DataFrame(x).to_csv('number.csv', index=False)
+
+        x = x.reshape((1, len(x)))
         prediction = model.predict_classes(x)
         return prediction[0]
 
