b2bde48effc98f548d4f8e7975f6819d50d9c9f8
Luis <ladm2110@gmail.com>
2018-09-08 17:15:10 -0400

    About page added.

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.errorhandler(404)
 def url_error(e):
     return """
