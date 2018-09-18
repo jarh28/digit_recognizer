1c780efefe2d4c72c4f1cef82b600c05193d5a7d
Luis <ladm2110@gmail.com>
2018-09-18 17:55:12 -0400

    Added setup.py for Openshift app

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='ws',
+    version='1.0',
+    description='Digit Recognizer',
+    author='Luis A. Denis Miranda',
+    author_email='ladm2110@gmail.com',
+    url='',
+    install_requires=['flask=1.0.2', 'flask_cors=3.0.4', 'opencv-python=3.4.2.17', 'tensorflow=1.10.1', 'numpy=1.14.3']
+)
