eb5e59aa6626a10b363595347edc226a1f7656e6
Luis <ladm2110@gmail.com>
2018-09-18 18:03:30 -0400

    Changes to setup file

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='ws',
     author='Luis A. Denis Miranda',
     author_email='ladm2110@gmail.com',
     url='',
-    install_requires=['flask=1.0.2', 'flask_cors=3.0.4', 'opencv-python=3.4.2.17', 'tensorflow=1.10.1', 'numpy=1.14.3']
+    install_requires=['flask==1.0.2', 'flask_cors==3.0.4', 'opencv-python==3.4.2.17', 'tensorflow==1.10.1', 'numpy==1.14.3'],
 )
